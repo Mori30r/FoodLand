a28cc466fbe3d1f19d8984cf4a33c12289951758
Mori 30R <ghahreman00@gmail.com>
2020-11-03 20:00:56 +0330

    images and dummy data for plans section

---
+++ src/dummyData.js
@@ -0,0 +1,21 @@
+import SpecPizza from './assets/images/spec-pizza.png'
+import VegPizza from './assets/images/veg-pizza.png'
+import MarPizza from './assets/images/margarita-pizza.png'
+
+export const data = [
+    {
+        name: 'Special Pizza',
+        image: SpecPizza,
+        rate: 5,
+    },
+    {
+        name: 'Vegetable Pizza',
+        image: VegPizza,
+        rate: 5,
+    },
+    {
+        name: 'Margarita Pizza',
+        image: MarPizza,
+        rate: 4,
+    }
+]
